92e7c5bea78bc9438ced23cf694023fec65ae2d2
vriti-shivang <63708886+vriti-shivang@users.noreply.github.com>
2020-07-21 22:51:02 +0530

    Add files via upload

---
+++ Day 1.py
@@ -0,0 +1,15 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+a=int(input())
+# Read and save an integer, double, and String to your variables.
+b=float(input())
+# Print the sum of both integer variables on a new line.
+print(i+a)
+# Print the sum of the double variables on a new line.
+print(b+d)
+# Concatenate and print the String variables on a new line
+t=input()
+# The 's' variable above should be printed first.
+print(s+t)
