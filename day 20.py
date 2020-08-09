fe32f91f621482a8b84eae9b89d9e04d53d6ef8c
vriti-shivang <63708886+vriti-shivang@users.noreply.github.com>
2020-08-09 23:18:19 +0530

    Add files via upload

---
+++ day 20.py
@@ -0,0 +1,11 @@
+    count_swap = 0
+    for num in range(len(arr)-1, 0, -1):
+        for i in range(num):
+            if arr[i] > arr[i+1]:
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+                count_swap +=1
+    return count_swap
+
+print "Array is sorted in {} swaps.".format(bubble(a))
+print "First Element: {}".format(a[0])
+print "Last Element: {}".format(a[-1])
